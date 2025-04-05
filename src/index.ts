a5500522e932ab7470815b1ec4049e9bcba14e15
Srikar Kulkacherla <kulkacherlasrikar@gmail.com>
2025-04-05 15:43:49 +0530

    global error handlers, swagger api docs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
+import { ResponseHelper } from './utils/ResponseHelper';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,16 @@ app.use(express.json());
 
 // Routes
 app.use('/runbook/api/v1', routes);
+
+// 404 Handler
+app.use((req, res) => {
+  ResponseHelper.sendNotFound(res);
+});
+
+// Global Error Handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  ResponseHelper.sendInternalServerError(res, err?.message, err);
+});
 
 // Start server
 app.listen(port, () => {
